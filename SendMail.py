72a3bf93d3e9bfd989bb879bdc1c60e40bdc1846
Jon Stødle <jonstodle@outlook.com>
2017-09-26 09:06:13 +0200

    Add server address as a parameter

--- SendMail.py
+++ SendMail.py
@@ -12,16 +12,17 @@ def send_mail(
         subject,
         body,
         username,
-        password
+        password,
+        server_address
 ):
-    """Sends an email to the specified receivers using the Sykehuspartner Exchange server."""
+    """Sends an email to the specified receivers using an Exchange server."""
     message = MIMEMultipart()
     message["From"] = sender
     message["To"] = ",".join(receivers)
     message["Subject"] = subject
     message.attach(MIMEText(body))
 
-    server = smtplib.SMTP("outlook.sykehuspartner.no", 587)
+    server = smtplib.SMTP(server_address, 587)
     server.starttls()
     server.login(username, password)
     server.sendmail(sender, receivers, message.as_string())
@@ -35,6 +36,7 @@ if __name__ == "__main__":
     parser.add_argument("body", type=str, help="Body text of the email. Can be plain text or a file path when -f is passed.")
     parser.add_argument("username", type=str, help="The username to authenticate with on mail server.")
     parser.add_argument("password", type=str, help="The password to authenticate with on mail server.")
+    parser.add_argument("server", type=str, help="The address of the mail server.")
     parser.add_argument("-f", "--file", action="store_true", help="Specifies that the body argument contains a file path instead of plain text")
     args = parser.parse_args()
 
@@ -42,4 +44,4 @@ if __name__ == "__main__":
         with open(args.body) as file:
             args.body = file.read()
 
-    send_mail(args.sender, args.receivers.split(","), args.subject, args.body, args.username, args.password)
+    send_mail(args.sender, args.receivers.split(","), args.subject, args.body, args.username, args.password, args.server)
