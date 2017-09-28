6a5bfaa805cba76b724da428ae83d504e64fa582
Jon Stødle <jonstodle@outlook.com>
2017-09-28 10:16:34 +0200

    Add PowerSocket script

---
+++ PowerSocket.py
@@ -0,0 +1,19 @@
+import Rpi.GPIO as GPIO
+import argparse
+
+GPIO.setmode(GPIO.BCM)
+
+def set_power(
+        port: int,
+        on: bool
+):
+    GPIO.setup(18, GPIO.OUT)
+    GPIO.output(18, on)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("port", type=int, help="Which port to output on")
+    parser.add_argument("on", type=bool, help="True for turning power on, false for off")
+    args = parser.parse_args()
+
+    set_power(args.port, args.on)
