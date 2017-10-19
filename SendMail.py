89947e49b9e41d07f2df10e37e22a897958ce4b6
Unknown <jonstodle@outlook.com>
2017-10-19 14:29:38 +0200

    Change SendMail to use an MX server to send mail

--- SendMail.py
+++ SendMail.py
@@ -5,26 +5,26 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from typing import List
 
+def sanitize_email_address(address):
+    return str.replace(address, "@", "[at]")
+
 
 def send_mail(
         sender,
         receivers: List[str],
         subject,
         body,
-        username,
-        password,
         server_address
 ):
-    """Sends an email to the specified receivers using an Exchange server."""
+    """Sends an email to the specified receivers using SMTP."""
     message = MIMEMultipart()
-    message["From"] = sender
-    message["To"] = ",".join(receivers)
+    message["From"] = sanitize_email_address(sender)
+    message["To"] = sanitize_email_address(",".join(receivers))
     message["Subject"] = subject
     message.attach(MIMEText(body))
 
-    server = smtplib.SMTP(server_address, 587)
-    server.starttls()
-    server.login(username, password)
+    server = smtplib.SMTP(server_address, 25)
+    server.helo(server_address)
     server.sendmail(sender, receivers, message.as_string())
     server.quit()
 
@@ -34,8 +34,6 @@ if __name__ == "__main__":
     parser.add_argument("receivers", type=str, help="Comma-seperated list of email addresses.")
     parser.add_argument("subject", type=str, help="Subject of the email.")
     parser.add_argument("body", type=str, help="Body text of the email. Can be plain text or a file path when -f is passed.")
-    parser.add_argument("username", type=str, help="The username to authenticate with on mail server.")
-    parser.add_argument("password", type=str, help="The password to authenticate with on mail server.")
     parser.add_argument("server", type=str, help="The address of the mail server.")
     parser.add_argument("-f", "--file", action="store_true", help="Specifies that the body argument contains a file path instead of plain text")
     args = parser.parse_args()
@@ -44,4 +42,4 @@ if __name__ == "__main__":
         with open(args.body) as file:
             args.body = file.read()
 
-    send_mail(args.sender, args.receivers.split(","), args.subject, args.body, args.username, args.password, args.server)
+    send_mail(args.sender, args.receivers.split(","), args.subject, args.body, args.server)
